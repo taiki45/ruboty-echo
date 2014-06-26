b5c871a9c4050a3b07ffdb2cd3d1f88e3ce36ec5
Taiki ONO <taiks.4559@gmail.com>
2014-06-26 22:24:43 +0900

    Implement ruboty echo

--- lib/ruboty/echo.rb
+++ lib/ruboty/echo.rb
@@ -1,7 +1,18 @@
-require "ruboty/echo/version"
+require 'ruboty'
+require 'ruboty/echo/version'
 
 module Ruboty
-  module Echo
-    # Your code goes here...
+  module Handlers
+    class Echo < Base
+      on(
+        /echo\s+(.*)/,
+        name: 'echo',
+        description: 'repeat your commnad'
+      )
+
+      def echo(message)
+        message.reply(message.match_data[1])
+      end
+    end
   end
 end
